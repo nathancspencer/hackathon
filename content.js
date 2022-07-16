1251a6893e192895c011dd6a89b4878991aee495
Stephen <stephen.chuang@student.unsw.edu.au>
2022-07-16 16:19:19 +1000

    Attempted to implement random reload.

--- content.js
+++ content.js
@@ -1,4 +1,36 @@
+
+
+
+
+var time = 8;
+
+// Calculates corruptedness to display.
+function corruptedness(time) {
+    return time * 1;
+}
+
+// Calculates time to randomly reload the page.
+function calcTimeToReload(time) {
+    return time * 1;
+}
+
+// Reload the page.
+function randomReload(time) {
+    location.reload(false);
+}
+
+// Replace all images with...
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+    images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+}
+
+// Image replacement.
 var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i+=2) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+    images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
 }
+
+// Randomly reload.
+window.setInterval('randomReload()', timeToReload)
+randomReload(time);
