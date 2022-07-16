ce5d582b13daba4c255676affd0ea8a755e8dc73
distrib134 <69830907+distrib134@users.noreply.github.com>
2022-07-16 21:08:09 +1000

    Delete content.js

--- content.js
+++
@@ -1,4 +0,0 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i+=2) {
-  images[i].src = 'https://loremflickr.com/' + images[i].width + '/' + images[i].height + '/earth';
-}
