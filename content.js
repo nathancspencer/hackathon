3e6387c9fa6d5a910a5e65c3a6b2273f2e355650
Stephen <stephen.chuang@student.unsw.edu.au>
2022-07-16 15:52:47 +1000

    Tried to implement random reloading.

--- content.js
+++ content.js
@@ -1,4 +1,23 @@
+
+// function randomReload() {
+    // Generate random number.
+    var randNum = Math.random();
+
+    // The constant for which random numbers are multiplied by.
+    var multiplier = 8;
+
+    // Calculate time until next reload.
+    var timeToReload = randNum * multiplier;
+
+    // Wait for timeToReload seconds.
+    setTimeout(timeToReload);
+
+    // Reload the page.
+    location.reload(false);
+// }
+
+
 var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+    images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
 }
